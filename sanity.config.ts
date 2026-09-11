3a72518ddb910b0efd3dd9d81742298df13358dc
vincentdigitals <omoseebivincent@gmail.com>
2026-09-11 21:23:06 +0100

    kkk

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
 import { schemaTypes } from "./src/sanity/schemaTypes";
 
 export default defineConfig({
@@ -6,5 +7,6 @@ export default defineConfig({
   title: "Vincent Portfolio",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "si3lnlfc",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production",
+  plugins: [structureTool()],
   schema: { types: schemaTypes },
 });
